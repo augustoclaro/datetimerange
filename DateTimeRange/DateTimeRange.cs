c966a5591f89d52893f2de746597d7a1049a3509
Augusto Claro <augustoclaro1@hotmail.com>
2016-02-09 23:19:19 -0200

    first Commit

---
+++ DateTimeRange/DateTimeRange.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DateTimeRange
+{
+    public class DateTimeRange
+    {
+        public DateTime StartDate { get; }
+        public DateTime EndDate { get; }
+        public TimeSpan Duration => EndDate - StartDate;
+
+        private DateTimeRange(DateTime? startDate, DateTime? endDate)
+        {
+            StartDate = startDate ?? DateTime.MinValue;
+            EndDate = endDate ?? DateTime.MaxValue;
+        }
+
+        public static DateTimeRange Create(DateTime? startDate = null, DateTime? endDate = null)
+            => new DateTimeRange(startDate, endDate);
+
+        public bool HasDate(DateTime date)
+            => StartDate <= date && EndDate >= date;
+
+        public bool HasDate(DateTime? date)
+            => date != null && HasDate(date.Value);
+
+        public bool HasRange(DateTimeRange range)
+            => HasDate(range.StartDate) || HasDate(range.EndDate)
+                || range.HasDate(StartDate) || range.HasDate(EndDate);
+
+        public bool HasRange(DateTime? startDate, DateTime? endDate)
+            => HasRange(Create(startDate, endDate));
+    }
+}
---
+++ DateTimeRange/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DateTimeRange.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTimeRange RangeUntil(this DateTime startDate, DateTime? endDate)
+            => DateTimeRange.Create(startDate, endDate);
+
+        public static DateTimeRange RangeUntil(this DateTime? startDate, DateTime? endDate)
+            => DateTimeRange.Create(startDate, endDate);
+
+        public static DateTimeRange RangeSince(this DateTime endDate, DateTime? startDate)
+            => DateTimeRange.Create(startDate, endDate);
+
+        public static DateTimeRange RangeSince(this DateTime? endDate, DateTime? startDate)
+            => DateTimeRange.Create(startDate, endDate);
+    }
+}
